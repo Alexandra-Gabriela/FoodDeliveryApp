e1487f780c368b666246475cb2b3c2113b804b90
Alexandra-Gabriela <alexandra.gabriela.gal@gmail.com>
2025-02-04 12:17:23 +0200

    .

---
+++ FoodDeliveryApp/src/main/java/org/fooddelivery/SpringBootDomain_FoodDeliveryApp.java
@@ -0,0 +1,28 @@
+package org.fooddelivery;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Logger;
+
+@SpringBootConfiguration
+public class SpringBootDomain_FoodDeliveryApp extends SpringBootServletInitializer {
+    private static Logger logger = Logger.getLogger(SpringBootDomain_FoodDeliveryApp.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Loading ... :: SpringBoot - FoodDeliveryApp Default Settings ...  ");
+        SpringApplication.run(SpringBootDomain_FoodDeliveryApp.class, args);
+    }
+    @Bean
+    public ModelMapper setupModelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setSkipNullEnabled(true);
+        return mapper;
+    }
+}
